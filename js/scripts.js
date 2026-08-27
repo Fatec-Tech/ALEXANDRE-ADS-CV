feb48d38c30be8af2f3baef57bab4a186f67590a
Alexandre Bertoni <alexandre.vfbertoni@gmail.com>
2026-08-27 16:51:39 -0300

    Sobre finalizado! Closes #2

--- js/scripts.js
+++ js/scripts.js
@@ -9,26 +9,69 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
+    // Bootstrap ScrollSpy
     const sideNav = document.body.querySelector('#sideNav');
+
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
             rootMargin: '0px 0px -40%',
         });
-    };
+    }
 
-    // Collapse responsive navbar when toggler is visible
+    // Navbar responsiva
     const navbarToggler = document.body.querySelector('.navbar-toggler');
+
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    responsiveNavItems.map(function (responsiveNavItem) {
+
+    responsiveNavItems.forEach(responsiveNavItem => {
+
         responsiveNavItem.addEventListener('click', () => {
+
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
+
         });
+
+    });
+
+
+    // Ícones sociais
+    const icons = document.querySelectorAll(".social-icon");
+
+    icons.forEach(icon => {
+
+        const elemento = icon.querySelector("i");
+
+        icon.addEventListener("mouseenter", () => {
+
+            elemento.style.setProperty("--cor1", corAleatoria());
+            elemento.style.setProperty("--cor2", corAleatoria());
+            elemento.style.setProperty("--cor3", corAleatoria());
+
+        });
+
+
+        icon.addEventListener("mousemove", (event) => {
+
+            const rect = icon.getBoundingClientRect();
+
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+
+            elemento.style.setProperty("--x", `${x}px`);
+            elemento.style.setProperty("--y", `${y}px`);
+
+        });
+
     });
 
 });
+
+
+function corAleatoria() {
+    return `hsl(${Math.random() * 360}, 100%, 50%)`;
+}
